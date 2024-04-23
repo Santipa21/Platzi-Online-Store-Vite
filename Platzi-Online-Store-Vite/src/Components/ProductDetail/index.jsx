afd8ad5efeb49ffe42b3a8ec80293e16188b05f8
Santi Patiño <santiph2000@gmail.com>
2024-04-22 20:08:46 -0500

    Change fake api and aside logic

--- Platzi-Online-Store-Vite/src/Components/ProductDetail/index.jsx
+++ Platzi-Online-Store-Vite/src/Components/ProductDetail/index.jsx
@@ -1,9 +1,14 @@
 import { XCircleIcon } from '@heroicons/react/24/solid'
 import './styles.css'
+import { useContext } from 'react'
+import { ShoppingCartContext } from "../../Context"
+
 
 const ProductDetail = () => {
+    const context = useContext(ShoppingCartContext)
+
     return(
-        <aside className="product-detail w-[360px] flex flex-col fixed bg-white right-0 border border-black rounded-lg top-[68px]">
+        <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail w-[360px] flex flex-col fixed bg-white right-0 border border-black rounded-lg top-[68px]`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
                 <div>
