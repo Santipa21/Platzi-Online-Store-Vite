a5f8235c8420bcaa705746b0ac216b08ecb70612
Santi Patiño <santiagopatino@flare.com.co>
2024-04-24 16:50:01 -0500

    shopping cart

--- Platzi-Online-Store-Vite/src/Components/Card/index.jsx
+++ Platzi-Online-Store-Vite/src/Components/Card/index.jsx
@@ -1,12 +1,12 @@
 import { useContext } from "react"
 import { ShoppingCartContext } from "../../Context"
-import { PlusCircleIcon } from '@heroicons/react/24/solid'
+import { PlusCircleIcon, CheckCircleIcon } from '@heroicons/react/24/solid'
 
 
 const Card = (data) => {
     const context = useContext(ShoppingCartContext)
 
-    const showProduct= (productDetail) => {
+    const showProduct = (productDetail) => {
         context.openProductDetail()
         context.setProductToShow(productDetail)
     }
@@ -17,20 +17,38 @@ const Card = (data) => {
         context.setCartProducts([...context.cartProducts, productData])
         context.openCheckoutSideMenu()
         context.closeProductDetail()
-        console.log(context.cartProducts)
     }
-    return(
+
+    const renderIcon = (id) => {
+        const isInCart = context.cartProducts.filter(product => product.id === id).length > 0
+
+        if (isInCart) {
+            return (
+                <div
+                    className="absolute top-0 right-0 flex justify-center items-center rounded-full bg-black w-6 h-6 m-2 p-1">
+                    <CheckCircleIcon className="h-6 w-6 text-white"></CheckCircleIcon>
+                </div>
+            )
+
+        }else{
+            return (
+            <div
+                    onClick={(event) => addProductsToCart(event, data.data)}
+                    className="absolute top-0 right-0 flex justify-center items-center rounded-full bg-white w-6 h-6 m-2 p-1">
+                    <PlusCircleIcon className="h-6 w-6 text-black"></PlusCircleIcon>
+                </div>
+        )}
+
+    }
+
+    return (
         <div
-        onClick={() => showProduct(data.data)}
-        className="bg-white cursor-pointer w-56  rounded-lg">
+            onClick={() => showProduct(data.data)}
+            className="bg-white cursor-pointer w-56  rounded-lg">
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">{data.data.category}</span>
                 <img className="w-full h-full object-contain rounded-lg " src={data.data.image} alt={data.data.title} />
-                <div 
-                onClick={(event) => addProductsToCart(event, data.data)}
-                className="absolute top-0 right-0 flex justify-center items-center rounded-full bg-white w-6 h-6 m-2 p-1">
-                    <PlusCircleIcon className="h-6 w-6 text-black"></PlusCircleIcon>
-                </div>
+                {renderIcon(data.data.id)}
             </figure>
             <p className="flex justify-between">
                 <span className="text-sm font-light">{data.data.title}</span>
--- Platzi-Online-Store-Vite/src/Components/CheckoutSideMenu/index.jsx
+++ Platzi-Online-Store-Vite/src/Components/CheckoutSideMenu/index.jsx
@@ -3,10 +3,16 @@ import './styles.css'
 import { useContext } from 'react'
 import { ShoppingCartContext } from "../../Context"
 import OrderCard from '../../Components/OrderCard'
+import { totalPrice } from '../../Utils'
 
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext)
+
+    const handleDelete = (id) => {
+        const filteredProducts = context.cartProducts.filter(product => product.id != id)
+        context.setCartProducts(filteredProducts)
+    }
 
     return (
         <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu w-[360px] flex flex-col fixed bg-white right-0 border border-black rounded-lg top-[68px]`}>
@@ -18,17 +24,25 @@ const CheckoutSideMenu = () => {
                     <XCircleIcon className="h-6 w-6 text-black cursor-pointer"></XCircleIcon>
                 </div>
             </div>
-            <div className='px-6'>
+            <div className='px-6 overflow-y-scroll'>
             {
                 context.cartProducts.map(product => (
                     <OrderCard 
                     key={product.id}
+                    id={product.id}
                     title={product.title}
                     imageUrl={product.image}
                     price={product.price}
+                    handleDelete={handleDelete}
                     />
                 ))
             }
+            </div>
+            <div className='px-6'>
+            <p className='flex justify-between items-center'>
+                <span className='font-light'>Total</span>
+                <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
+            </p>
             </div>
         </aside>
     )
